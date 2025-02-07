533bbd9344adbc472f8ae90158dd4fd594b71cf3
dianewudw <diane@Dianes-Air.localdomain>
2025-02-07 16:39:33 -0600

    tanstack router- client-side routing added

---
+++ padre-ginos/src/Header.jsx
@@ -0,0 +1,19 @@
+import { Link } from '@tanstack/react-router'
+import { useContext } from 'react'
+import { CartContext } from './contexts'
+
+export default function Header(){
+    
+    const[cart] = useContext(CartContext)
+
+    return(
+        <nav>
+            <Link>
+                <h1 className="logo">Padre Gino's Pizza</h1>
+            </Link>
+                <div className="nav-cart">
+                    <span className="nav-cart-number">{cart.length}</span>
+                </div>
+        </nav>
+    )
+}
