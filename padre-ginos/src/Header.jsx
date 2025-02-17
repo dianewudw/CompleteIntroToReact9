098830ac437a9446c28103a6df22809a817b3964
dianewudw <diane@Dianes-Air.localdomain>
2025-02-17 07:36:41 -0600

    updated testing for vitest

--- padre-ginos/src/Header.jsx
+++ padre-ginos/src/Header.jsx
@@ -2,6 +2,7 @@ import { Link } from '@tanstack/react-router'
 import { useContext } from 'react'
 import { CartContext } from './contexts'
 
+
 export default function Header(){
     
     const[cart] = useContext(CartContext)
@@ -12,7 +13,7 @@ export default function Header(){
                 <h1 className="logo">Padre Gino's Pizza</h1>
             </Link>
                 <div className="nav-cart">
-                    <span className="nav-cart-number">{cart.length}</span>
+                    <span className="nav-cart-number" data-testid="cart-number">{cart.length}</span>
                 </div>
         </nav>
     )
